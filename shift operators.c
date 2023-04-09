c16f3600222ef20fc574d56c47f9348b7d37820c
amarja04 <125913981+amarja04@users.noreply.github.com>
2023-04-10 04:02:21 +0530

    Add files via upload

---
+++ shift operators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+int main()  
+{  
+    int a=5; // variable initialization  
+    printf("The value of a<<2 is : %d\n ", a<<2);  
+  
+    int b=7; // variable initialization  
+    printf("The value of b>>2 is : %d ", b>>2);  
+    return 0;  
+}  
